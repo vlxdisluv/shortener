f87641426aaf5285a6213b6a8f02ec65cd12e89d
vlxdisluv <vlxdisluv@gmail.com>
2025-03-25 09:41:42 +0500

    refactor(server): migrate from net/http mux to chi router

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/vlxdisluv/shortener/internal/app/handlers"
 	"github.com/vlxdisluv/shortener/internal/app/storage"
 	"log"
@@ -11,11 +13,16 @@ func Start(addr string) {
 	repo := storage.NewInMemoryURLStore()
 	shortURLHandler := handlers.NewShortURLHandler(repo)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", shortURLHandler)
+	r := chi.NewRouter()
 
-	log.Printf("Starting server on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Post("/", shortURLHandler)
+	r.Get("/{hash}", shortURLHandler)
+
+	if err := http.ListenAndServe(addr, r); err != nil {
 		log.Fatalf("server failed to start: %v", err)
 	}
 }
