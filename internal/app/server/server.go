3857c4500ed31ce970852e4eb518e56ba6ff50be
Vladislav Severin <vlxdisluv@gmail.com>
2025-09-13 12:36:59 +0500

    feat: add Postgres connection and DB ping health endpoint (#11)

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,23 +1,43 @@
 package server
 
 import (
+	"context"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/vlxdisluv/shortener/config"
 	"github.com/vlxdisluv/shortener/internal/app/handlers"
 	"github.com/vlxdisluv/shortener/internal/app/logger"
 	customMiddleware "github.com/vlxdisluv/shortener/internal/app/middleware"
 	"github.com/vlxdisluv/shortener/internal/app/storage"
 	"go.uber.org/zap"
+	"log"
 	"net/http"
 )
 
+//var db *pgxpool.Pool
+
 func Start(cfg *config.Config) {
+	fmt.Printf("%+v\n", cfg)
+	fmt.Printf("Ddb connection DSN %s \n", cfg.DatabaseDSN)
+	poolConfig, err := pgxpool.ParseConfig(cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatalln("Unable to parse DATABASE_URL:", err)
+	}
+
+	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+	if err != nil {
+		log.Fatalln("Unable to create connection pool:", err)
+	}
+
 	repo, err := storage.NewInMemoryURLStore(cfg.FileStoragePath)
 	if err != nil {
 		logger.Log.Fatal("server failed to init storage", zap.Error(err))
 	}
+
 	h := handlers.NewShortURLHandler(repo)
+	hh := handlers.NewHealthHandler(db)
 
 	r := chi.NewRouter()
 
@@ -29,6 +49,7 @@ func Start(cfg *config.Config) {
 	r.Post("/", h.CreateShortURLFromRawBody)
 	r.Get("/{hash}", h.GetShortURL)
 	r.Post("/api/shorten", h.CreateShortURLFromJSON)
+	r.Get("/ping", hh.DBHealth)
 
 	logger.Log.Info("Server started successfully",
 		zap.String("address", cfg.Addr),
